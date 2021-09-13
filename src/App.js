116a7b00e0e63d43e86565438095bc1f4065356e
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-13 18:13:13 -0300

    encapsulando componentes no BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
     </div>
+    </BrowserRouter>
   );
 }
 
