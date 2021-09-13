489f3307a8675a3a1d4766c0a83f7cbc58cf8b58
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-09-13 18:20:00 -0300

    rota / ok

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      {/* <Pokedex pokemons={pokemons} /> */}
+      <Route to='/' render={(rotProp)=> <Pokedex {...rotProp} pokemons={pokemons}/>}/>
     </div>
     </BrowserRouter>
   );
